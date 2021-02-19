cb82327cdcd199c6edd07c19be51bb28f7eaf6b7
zhangliming <1649172879@gmail.com>
2021-02-19 21:42:12 +0800

    refine

--- src/main/java/arithmetic/ReverseString.java
+++ src/main/java/arithmetic/ReverseString.java
@@ -2,14 +2,23 @@ package arithmetic;
 
 public class ReverseString {
 
-    class Solution {
-        public void reverseString(char[] s) {
-            int n = s.length;
-            for (int left = 0, right = n - 1; left < right; ++left, --right) {
-                char tmp = s[left];
-                s[left] = s[right];
-                s[right] = tmp;
-            }
+    public static void main(String[] args) {
+
+        String name = "zhangliming";
+
+        char[] s = name.toCharArray();
+
+        reverseString(s);
+
+        System.out.println(s);
+    }
+
+    public static void reverseString(char[] s) {
+        int n = s.length;
+        for (int left = 0, right = n - 1; left < right; ++left, --right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
         }
     }
 }
