0cbc49ae47e617e531c0ece3bb9e281141860303
dawn-mac <747395654@qq.com>
2021-03-17 09:04:14 +0800

    ABC condition

---
+++ src/main/java/aqs/ABC_Lock.java
@@ -0,0 +1,76 @@
+package aqs;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ABC_Lock {
+    // 通过JDK5中的Lock锁来保证线程的访问的互斥
+    private static int state = 0;
+    //通过state的值来确定是否打印
+    private static Lock lock = new ReentrantLock();
+
+    static class ThreadA extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10;) {
+                try {
+                    lock.lock();
+                    // 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                    while (state % 3 == 0) {
+                        System.out.print("A");
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    lock.unlock();// unlock()操作必须放在finally块中
+                }
+            }
+        }
+    }
+
+    static class ThreadB extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10;) {
+                try {
+                    lock.lock();
+                    // 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                    while (state % 3 == 1) {
+                        System.out.print("B");
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    // unlock()操作必须放在finally块中
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    static class ThreadC extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 10;) {
+                try {
+                    lock.lock();
+                    // 多线程并发，不能用if，必须用循环测试等待条件，避免虚假唤醒
+                    while (state % 3 == 2) {
+                        System.out.print("C");
+                        state++;
+                        i++;
+                    }
+                } finally {
+                    // unlock()操作必须放在finally块中
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ThreadA().start();
+        new ThreadB().start();
+        new ThreadC().start();
+    }
+}
