b7a433feab5eb4935029a30e78fe1bcdb389aad6
zhangliming <1649172879@gmail.com>
2021-03-22 20:39:11 +0800

    threadlocalmap

---
+++ src/main/java/aqs/TestThreadLocal.java
@@ -0,0 +1,15 @@
+package aqs;
+
+public class TestThreadLocal {
+
+    private static ThreadLocal T = new ThreadLocal();
+    private static ThreadLocal T1 = new InheritableThreadLocal();
+
+    //然后我们将ThreadLocal换成InheritableThreadLocal
+
+    public static void main(String[] args) {
+        T.set("23abc");
+        System.out.println("main" + T.get());
+        new Thread(() -> System.out.println("sub" + T.get())).start();
+    }
+}
